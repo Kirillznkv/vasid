522b9046f6f2f9622c27cc2d9dbcf538d6df5585
kshanti_home <Zenich99@gmail.com>
2021-02-11 21:35:31 +0300

    rhkrhjgjilsrgv

---
+++ p64z13.c
@@ -0,0 +1,35 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+int		main()
+{
+	char	*filename = "test.txt";
+	FILE	*file;
+	char	*line;
+
+// Подсчет символов
+	int i = 0;
+	file = fopen(filename, "r");
+	if (file == NULL)
+	{
+		printf("Не удалось открыть файл\n");
+		return (1);
+	}
+	while (fgetc(file) != EOF)
+		i++;
+	fclose(file);
+// Считывание файла
+	file = fopen(filename, "r");
+	if (file == NULL)
+	{
+		printf("Не удалось открыть файл\n");
+		return (1);
+	}
+	line = (char*)malloc(i + 1);
+	line[i--] = '\0';
+	while ((line[i] = fgetc(file)) != EOF)
+		i--;
+	printf("%s", line);
+	fclose(file);
+	return (0);
+}
